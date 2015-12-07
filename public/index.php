43d68368e4a123819958191fe5d8998ac3ccff59
Mark van der Laan <markkvdlaan93@gmail.com>
2015-12-07 18:54:15 +0100

    aanmaken menu
    Het menu onder de top-header

--- public/index.php
+++ public/index.php
@@ -5,22 +5,22 @@
 	<header>
 		<div class="row">
 			<div class="col-6 col-m-6 topheader">
-				<ul class="topheader-menu-1">
-					<li>+565 975 658</li>
-					<li>mail@markvanderlaan.nl</li>
-				</ul>
-			</div>
-			<div class="col-6 col-m-6 topheader">
-				<ul class="topheader-menu-2 topheader-menu-m-2">
-					<li>Latest News</li>
-					<li>Login</li>
-					<li>Register</li>
-					<li>About Us</li>
-				</ul>
+					<ul class="topheader-menu-1">
+						<li>+565 975 658</li>
+						<li>mail@markvanderlaan.nl</li>
+					</ul>
+				</div>
+				<div class="col-6 col-m-6 topheader">
+					<ul class="topheader-menu-2 topheader-menu-m-2">
+						<li>Latest News</li>
+						<li>Login</li>
+						<li>Register</li>
+						<li>About Us</li>
+					</ul>
+				</div>
 			</div>
 		</div>
-	</div>
-</header>
+	</header>
 <!-- END HEADER -->
 <!-- BRAND NAME -->
 <div class="row">
@@ -34,10 +34,22 @@
 <!-- END BRAND NAME -->
 <!-- IMAGE HEADER -->
 <div class="row">
-		<img src="images/image-header.jpg" alt="image-header">
-
+	<img src="images/image-header.jpg" alt="image-header">
 </div>
 <!-- END IMAGE HEADER -->
+<!-- MENU -->
+<div class="row">
+	<div class="col-12 col-m-12">
+		<ul class="menu">
+			<li>Home</li>
+			<li>Diensten</li>
+			<li>Portfolio</li>
+			<li>Team</li>
+			<li>Contact</li>
+		</ul>
+	</div>
+</div>
+<!-- END MENU -->
 
 
 </body>
