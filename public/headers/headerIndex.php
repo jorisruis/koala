fcfc9486b0fa9ec367c86a24b74011e178feac86
Mark van der Laan <markkvdlaan93@gmail.com>
2015-12-07 14:54:04 +0100

    Invoegen javascript and stylesheet

--- public/headers/headerIndex.php
+++ public/headers/headerIndex.php
@@ -2,6 +2,11 @@
 <head>
 	<title>Panda Web development</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<!-- CSS stylesheets -->
+	
+	<!-- Custom stylesheets -->
 	<link rel="stylesheet" type="text/css" href="css/theme.css">
+	
+	<!-- jQuery & custom JS -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="js/smoothSlider.js"></script>
 </head>
