5842b459ec388dc1a5a9223818630a7b19f75d3e
Mark van der Laan <markkvdlaan93@gmail.com>
2015-12-07 18:04:12 +0100

    Brand-name toegevoegd eerste opzet

--- public/index.php
+++ public/index.php
@@ -1,25 +1,38 @@
 <?php include('headers/headerIndex.php') ?>
 
 <body>
+	<!-- HEADER -->
 	<header>
 		<div class="row">
 			<div class="col-6 col-m-6 topheader">
-					<ul class="topheader-menu-1">
-						<li>+565 975 658</li>
-						<li>mail@markvanderlaan.nl</li>
-					</ul>
-				</div>
-				<div class="col-6 col-m-6 topheader">
-					<ul class="topheader-menu-2 topheader-menu-m-2">
-						<li>Latest News</li>
-						<li>Login</li>
-						<li>Register</li>
-						<li>About Us</li>
-					</ul>
-				</div>
+				<ul class="topheader-menu-1">
+					<li>+565 975 658</li>
+					<li>mail@markvanderlaan.nl</li>
+				</ul>
+			</div>
+			<div class="col-6 col-m-6 topheader">
+				<ul class="topheader-menu-2 topheader-menu-m-2">
+					<li>Latest News</li>
+					<li>Login</li>
+					<li>Register</li>
+					<li>About Us</li>
+				</ul>
+			</div>
 		</div>
 	</div>
 </header>
+<!-- END HEADER -->
+<!-- BRAND NAME -->
+<div class="row">
+	<div class="col-12 col-m-12">
+		<div id="brand-name">
+			<h1>Koala</h1>
+			<h3>Webdevelopment</h3>
+		</div>
+	</div>
+</div>
+<!-- END BRAND NAME -->
+
 
 </body>
 
