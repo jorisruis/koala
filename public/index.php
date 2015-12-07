6ef5b13ae55708b7835adfa9480b4d2d66412758
Mark van der Laan <markkvdlaan93@gmail.com>
2015-12-07 17:52:01 +0100

    Eerste opzet topheader

--- public/index.php
+++ public/index.php
@@ -3,23 +3,23 @@
 <body>
 	<header>
 		<div class="row">
-			<div class="col-6 col-m-6">
-				<ul class="first-menu">
-					<li>+565 975 658</li>
-					<li>mail@markvanderlaan.nl</li>
-					<li>Monday - Friday, 8.00 - 20.00</li>
-				</ul>
-			</div>
-			<div class="col-6 col-m-6">
-				<ul class="second-menu">
-					<li>Latest News</li>
-					<li>Login</li>
-					<li>Register</li>
-					<li>About Us</li>
-				</ul>
-			</div>
+			<div class="col-6 col-m-6 topheader">
+					<ul class="topheader-menu-1">
+						<li>+565 975 658</li>
+						<li>mail@markvanderlaan.nl</li>
+					</ul>
+				</div>
+				<div class="col-6 col-m-6 topheader">
+					<ul class="topheader-menu-2">
+						<li>Latest News</li>
+						<li>Login</li>
+						<li>Register</li>
+						<li>About Us</li>
+					</ul>
+				</div>
 		</div>
-	</header>
+	</div>
+</header>
 
 </body>
 
