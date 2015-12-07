48dfbd71bde77994dd3fdf39a9de8f2c8a1c027f
Mark van der Laan <markkvdlaan93@gmail.com>
2015-12-07 14:49:55 +0100

    indeling stylesheet
    inclusief toevoegen

--- public/headers/headerIndex.php
+++ public/headers/headerIndex.php
@@ -2,4 +2,6 @@
 <head>
 	<title>Panda Web development</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- CSS stylesheets -->
+	<link rel="stylesheet" type="text/css" href="css/theme.css">
 </head>
