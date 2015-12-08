7eb2b97b64817cb97b3ae952022c3897df950fc4
wesley-liefhebber <wesley.liefhebber@hotmail.com>
2015-12-08 15:15:54 +0100

    Brand volledig doorgevoerd, update logo

--- public/headers/headerIndex.php
+++ public/headers/headerIndex.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Panda Web development</title>
+	<title>Mixture Webdevelopment</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	
 	<!-- Google fonts -->
